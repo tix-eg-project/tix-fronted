cf59a51a77828c31c539ce4b8bc3c607f49d7dca
abdelrahmanmahfouz55 <abdelrahman.mahfouz@stampire.com>
2026-08-15 18:27:09 +0300

    feat: add GoogleOAuthProvider component to wrap application with Google authentication context

--- components/GoogleAuthProvider.tsx
+++ components/GoogleAuthProvider.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
-const CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ''
+const CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '685809250674-bnqkpu84bjk8et5r1pa04o4dt06ujl0k.apps.googleusercontent.com'
 
 export default function GoogleAuthProvider({ children }: { children: React.ReactNode }) {
   return (
